158987ee6965c79cf8cdf39298f2586d1f4f01dc
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-10 19:17:56 +0200

    done about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi doloribus totam debitis ut, libero, quasi eveniet nostrum veniam iure reiciendis placeat quam! Dolores quae cumque beatae, dicta optio soluta laborum.This is a simple app built with Next.js and Tailwind CSS.This is a simple app built with Next.js and Tailwind CSS.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi doloribus totam debitis ut, libero, quasi eveniet nostrum veniam iure reiciendis placeat quam! Dolores quae cumque beatae, dicta optio soluta laborum.This is a simple app built with Next.js and Tailwind CSS.This is a simple app built with Next.js and Tailwind CSS.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi doloribus totam debitis ut, libero, quasi eveniet nostrum veniam iure reiciendis placeat quam! Dolores quae cumque beatae, dicta optio soluta laborum.This is a simple app built with Next.js and Tailwind CSS.This is a simple app built with Next.js and Tailwind CSS.
+        </p>
+    </div>
+  )
+}

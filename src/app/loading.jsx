1a50c2f0e8334611cf59c2da203c48b39d5fafa4
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-15 11:28:15 +0200

    done loader

---
+++ src/app/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center'>
+      <img src="spinner.svg" alt="loading..." />
+      </div>
+  )
+}
+
+
+

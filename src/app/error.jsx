0b6f2a7fbb96ad571e8ae893ac13009fe2c99059
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-15 10:15:15 +0200

    done error page

---
+++ src/app/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useEffect } from 'react';
+
+export default function Error({ error, reset }) {
+        useEffect(() => {
+                console.log(error);
+        }, [error]);
+    return (
+        <div>error</div>
+    )
+}

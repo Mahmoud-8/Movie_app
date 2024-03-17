6d8f356cba38c71a245ce2e34eb63f36ac779759
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-17 13:13:11 +0200

    done card img

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
-import Link from '@next/Link';
+import Link from 'next/link';
 
-export default function Card(result) {
-  return (
-    <div>
-        <Link href={`/movie/${result.id}`}>
-            <Image
-                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path  || result.poster_path}`}
-                width={500}
-                height={300}
-                className='sm:rounded-lg'
-                >
-            </Image>
-        </Link>
-    </div>
-  )
+export default function Card({result}) {
+    return (
+        <div>
+                <Link href={`/movie/${result.id}`}>
+                        <Image
+                                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path  || result.poster_path}`}
+                                width={500}
+                                height={300}
+                                className='sm:rounded-lg'
+                                alt=""
+                                >
+                        </Image>
+                </Link>
+        </div>
+    )
 }

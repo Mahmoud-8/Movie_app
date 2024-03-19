44b84e4fce9338d7cd126e8818074481e3f6d0e8
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-19 17:24:56 +0200

    done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Card({result}) {
-    return (
-        <div>
-                <Link href={`/movie/${result.id}`}>
-                        <Image
-                                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path  || result.poster_path}`}
-                                width={500}
-                                height={300}
-                                className='sm:rounded-t-lg 
+export default function Card({ result }) {
+        return (
+                <div>
+                        <Link href={`/movie/${result.id}`}>
+                                <Image
+                                        src={`https://image.tmdb.org/t/p/original/${result.backdrop_path ||
+                                         result.poster_path}`}
+                                        width={500}
+                                        height={300}
+                                        className='sm:rounded-t-lg 
                                 group-hover:opacity-75
                                 transition-opacity duration-300'
-                                alt=""
+                                        alt=""
                                 >
-                        </Image>
-                        
-                </Link>
-        </div>
-    )
+                                </Image>
+                                <div className="">
+                                        <p className="line-clamp-2 text-md">{result.overview}</p>
+                                </div>
+
+                        </Link>
+                </div>
+        );
 }

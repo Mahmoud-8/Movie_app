1c1ed23271cab9f03d98e30476570f0dcd62bd64
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2024-03-20 10:45:33 +0200

    fetch the movies

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-export default function MoviePage() {
+export default async function MoviePage({params}) {
+    const movieId = params.id;
+    
+    // Fetch the data for the movie with the given ID
+    const res = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?
+    api_key=${process.env.API_KEY}&language=en-US`);
+    const data = await res.json();
   return (
     <div>MoviePage</div>
   )
